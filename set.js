cfc5943fd3df028080c88760133b4a1381db3ddd
amosi12 <220756773+amosi12@users.noreply.github.com>
2025-10-09 02:05:52 +0300

    Update set.js

--- set.js
+++ set.js
@@ -7,41 +7,32 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-
-module.exports = {
-    session: process.env.SESSION_ID || '',
-    PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "B.M.B-TECH",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "25576786×××××",              
+module.exports = { session: process.env.SESSION_ID || '',
+    PREFIXE: process.env.PREFIX || ".",
+    OWNER_NAME: process.env.OWNER_NAME || "bmb tevh",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "bmb xmd",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'B.M.B-TECH',
-    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/8qq3l4.jpg',
+    BOT : process.env.BOT_NAME || 'BMB-TECH',
+    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/zotx9t.jpg',
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-    HEROKU_API_KEY : process.env.HEROKU_API_KEY, 
-    WARN_COUNT : process.env.WARN_COUNT || '3',
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
     ETAT : process.env.PRESENCE || '',
-    ANTICALL : process.env.ANTICALL || 'yes',   
-    AUTO_BIO : process.env.AUTO_BIO || 'yes',               
+    CHATBOT : process.env.PM_CHATBOT || 'no',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    ANTIDELETE1 : process.env.ANTI_DELETE_MESSAGE || 'no',
-    AUTO_REACT : process.env.AUTO_REACT || 'no',
-    AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'yes',
-    AUTO_READ : process.env.AUTO_READ || 'yes',
-    AUTO_SAVE_CONTACTS : process.env.AUTO_SAVE_CONTACTS || 'yes',
-    CHATBOT: process.env.CHATBOT || 'yes',
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
-        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway"
-        : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+   
 };
-
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
     fs.unwatchFile(fichier);
     console.log(`mise à jour ${__filename}`);
     delete require.cache[fichier];
     require(fichier);
-});
+})
